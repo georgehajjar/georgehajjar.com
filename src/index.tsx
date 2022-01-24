70fbf4db04b9d27cfb7f041a47e61908ffbf5db8
George Hajjar <georgehajjar99@gmail.com>
2022-01-23 20:00:17 -0500

    connect to firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCrRX9K9fMWeMmc81r6CgwnL6Y_O5Hhr7k',
+  authDomain: 'georgehajjar-c12c6.firebaseapp.com',
+  projectId: 'georgehajjar-c12c6',
+  storageBucket: 'georgehajjar-c12c6.appspot.com',
+  messagingSenderId: '1078622997587',
+  appId: '1:1078622997587:web:e4455c17f4c0c846a70aa6',
+  measurementId: 'G-7CYXSK8KW6',
+};
+
+const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
